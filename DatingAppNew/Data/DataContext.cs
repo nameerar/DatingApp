d32e73d2fb7bf91d2ecb95ce1d2d016d9f610b93
Nameer Rasheed <nameerar2@outlook.com>
2019-11-08 10:19:28 +0530

    initial Commit

---
+++ DatingAppNew/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DatingAppNew.Models;
+
+namespace DatingAppNew.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Values> Values { get; set; }
+
+
+
+    }
+}
